64fdcced0aadb0c9a75ffba447e20a1cd665adb1
Steven Ufkes <sufkes@graphics.ccm.sickkids.ca>
2020-06-25 12:56:27 +0000

    finished brainOrHeart.sh

--- misc/brainOrHeart.sh
+++ misc/brainOrHeart.sh
@@ -2,6 +2,7 @@
 
 in_dir="$1"
 
+
 ## Define brain keywords
 brain_string='sss|dti|diffusion|ax-t1-3d|cor-t1-3d|sag-t1-3d|ax-3d-t1|cor-3d-t1|sag-3d-t1|ax-t2|cor-t2|sag-t2|ax-t1-mpr|cor-t1-mpr|sag-t1-mpr|ax-t1-tse|cor-t1-tse|sag-t1-tse|ax-t1-flash|cor-t1-flash|sag-t1-flash|ax-3d-mpr|cor-3d-mpr|sag-3d-mpr|ax-ssfp-3d|cor-ssfp-3d|sag-ssfp-3d|ax-3d-ssfp|cor-3d-ssfp|sag-3d-ssfp|svs|mag_image|pha_image|mip_image|swi_image'
 # superior saggital sinus (SSS)
@@ -26,13 +27,37 @@ heart_string='ventricles|svc|mpa|lpa|rpa|dao|aao|uv|2ch|3ch|4ch' # tf2d15_2ch, t
 # 'Standard' cardiac views for the left ventricle (LV) typically comprise 2-chamber, 3-chamber, 4-chamber, short-axis (SAX) views, or coronal LVOT. Right ventricle (RV) views typically comprise RVOT and RV 2-chamber views.
 
 # Brain series
-echo "Brain series:"
-ls "$in_dir" | grep -Ei "$brain_string" | grep -Eiv "$heart_string"
+#echo "Brain series:"
+#ls "$in_dir" | grep -Ei "$brain_string" | grep -Eiv "$heart_string"
+num_brain=$(ls "$in_dir" | grep -Ei "$brain_string" | grep -Eiv "$heart_string" | wc -l)
 
 # Heart series
-echo "Heart series:"
-ls "$in_dir" | grep -Ei "$heart_string" | grep -Eiv "$brain_string"
+#echo "Heart series:"
+#ls "$in_dir" | grep -Ei "$heart_string" | grep -Eiv "$brain_string"
+num_heart=$(ls "$in_dir" | grep -Ei "$heart_string" | grep -Eiv "$brain_string" | wc -l)
 
 # Unknown
-echo "Unknown series:"
-ls "$in_dir" | grep -Eiv "$heart_string" | grep -Eiv "$brain_string"
+#echo "Unknown series:"
+#ls "$in_dir" | grep -Eiv "$heart_string" | grep -Eiv "$brain_string"
+
+if [ $num_brain -gt 0 ]
+then
+    if [ $num_heart -gt 0 ]
+    then
+	scan_type="C"
+    else
+	scan_type="B"
+    fi
+elif [ $num_heart -gt 0 ]
+then
+    scan_type="H"
+else
+    scan_type="N"
+fi
+
+#echo "Scan type:" $scan_type
+echo $scan_type
+    
+    
+	   
+	      
